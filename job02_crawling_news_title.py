cd047a826822a4d79fec0d867e81ce2efdb7605e
MaRKim97 <153490752+MaRKim97@users.noreply.github.com>
2024-01-24 15:37:21 +0900

    Add files via upload

--- job02_crawling_news_title.py
+++ job02_crawling_news_title.py
@@ -1,26 +1,29 @@
+#팔월드 하고 싶다..
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import StaleElementReferenceException
 import pandas as pd
 import re
 import time
 import datetime
 
-category = ['Politics', 'Economy', 'Social', 'Culture', 'World', 'IT']
-pages = [105, 105, 105, 81, 105, 81]
+category = ['Politics', 'Economic', 'Social', 'Culture', 'World', 'IT']
 
 options = ChromeOptions()
-user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
 options.add_argument('user_agent=' + user_agent)
 options.add_argument('lang=ko_KR')
 
 service = ChromeService(executable_path=ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=options)
+pages = [105, 105, 105, 81, 105, 81]
 
-df_title = pd.DataFrame()
-for l in range(6):
+df_titles = pd.DataFrame()
+for l in range(0, 2):
     section_url = 'https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=10{}'.format(l)
     titles = []
     for k in range(1, pages[l]):
@@ -29,19 +32,22 @@ for l in range(6):
             driver.get(url)
             time.sleep(0.5)
         except:
-            print('driver.get', l, k)
+            print('drive.get',l, k)
+
         for i in range(1, 5):
             for j in range(1, 6):
                 try:
-                    title = driver.find_element('xpath', '//*[@id="section_body"]/ul[{}]/li[{}]/dl/dt[2]/a'.format(i, j)).text
-                    title = re.compile('[^가-힣]').sub(' ', title)
+                    title = driver.find_element('xpath', '//*[@id="section_body"]/ul[{}]/li[{}]/dl/dt[2]/a'.format(i,j)).text
+                    title = re.compile('[^가-힣]').sub(' ',title)
                     titles.append(title)
                 except:
-                    print('find element', l, k, i, j)
+                    print('find element',l, k, i, j)
+
         if k % 5 == 0:
-            print(l, k)
-            df_section_titles = pd.DataFrame(titles, columns=['titles'])
-            df_section_titles['category'] = category[l]
-            df_section_titles.to_csv('./crawling_data/data_{}_{}.csv'.format(l, k))
-            # df_title = pd.concat([df_title, df_section_titles], axis='rows', ignore_index=True)
+            print(k)
+            df_section_title = pd.DataFrame(titles, columns=['titles'])
+            df_section_title['category'] = category[l]
+            df_section_title.to_csv('./crawling_data/data_{}_{}.csv'.format(l, k))
+            df_titles = pd.concat([df_titles, df_section_title], axis=0, ignore_index=True)
+
 driver.close()
